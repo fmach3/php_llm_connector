a3f04ca0dbd759e138cbe41259f6aa954590e8bc
fmach3 <115867875+fmach3@users.noreply.github.com>
2025-02-07 01:05:09 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<form action="process.php" method="post">
+  <label for="input">Input Text:</label>
+  <textarea name="input" id="input" rows="4" cols="50"></textarea>
+  <input type="submit" value="Submit">
+</form>
+
